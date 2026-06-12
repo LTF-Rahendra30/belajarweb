c961a116cc01d9931365487c8a8520c4cbd57bd9
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-12 15:55:23 +0700

    feat: Try Infinity and Nan and write simple explanation

--- javascript/01_baisc_syntax/04_data_type.js
+++ javascript/01_baisc_syntax/04_data_type.js
@@ -1,2 +1,16 @@
+
+// String
 const text = "Helooo Everyoneeee This is Stringggg"
-console.log(text);
+console.log(text)
+
+// Num
+const year = 2026
+const weight = 63.5
+
+// IINFINITY
+const result = 20 /0 // This is special, Infinity, when we did aritmetic operation undefined, like dividing a velue by zero
+console.log(result)
+
+// NaN (Not A Number)
+const result2 = Number('Bob Marley') // Try change string to number, and then the result is "NaN" because we try String that isnt Number to change Number 
+console.log(result2)
