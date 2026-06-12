99d6543dd38b867ffb77e06c226234e7c216c220
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-12 16:23:36 +0700

    feat: Try change num and boolean to string

--- javascript/01_baisc_syntax/05_changed_value_between_data_type.js
+++ javascript/01_baisc_syntax/05_changed_value_between_data_type.js
@@ -1,3 +1,19 @@
 // Change to string with explicit Controversion
 
-const number 
+const number = 123
+const boolean = true
+// Before Change
+console.log(typeof number); 
+console.log(typeof boolean); 
+// Output: 
+// number
+// boolean
+const strNumber = String(number)
+const strBoolean = boolean.toString()
+
+// After
+console.log(typeof strNumber);
+console.log(typeof strBoolean); 
+// Output: 
+// string
+// string
