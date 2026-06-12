e6a375d3a8af10ea3d0db484cb844786515c995f
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-12 11:28:09 +0700

    feat: Try change variable type 'let'

--- javascript/01_baisc_syntax/variable.js
+++ javascript/01_baisc_syntax/variable.js
@@ -3,3 +3,12 @@ let username = "Bob";
 
 console.log(id)
 console.log(username)
+
+// Try change let variable type
+
+let name = "Adam";
+console.log("Before change: ",name);
+name = "Bob";
+console.log("After change: ",name);
+
+
