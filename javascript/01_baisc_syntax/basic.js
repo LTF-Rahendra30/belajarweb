d46204ebd75a81fdc8add9f5de3621eb7d78ed6b
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-12 11:13:33 +0700

    feat: Write control flow if else and try run

--- javascript/01_baisc_syntax/basic.js
+++ javascript/01_baisc_syntax/basic.js
@@ -1,4 +1,4 @@
-window.alert("Hello World");
+// window.alert("Hello World");
 
 console.log("Hello Everyone! ")
 
@@ -6,3 +6,12 @@ function sub(a,b) {
     return a + b;
 }
 console.log(sub(10,90));
+
+const balance = 100;
+
+if (balance < 100){
+    console.log("Not Eligible");
+}
+else{
+    console.log("Correct");
+}
