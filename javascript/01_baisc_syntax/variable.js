087b6e2cc72131679a658aa0fd5945e151a4c956
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-12 11:31:29 +0700

    feat: Try to change statement const variable type, and fail

--- javascript/01_baisc_syntax/variable.js
+++ javascript/01_baisc_syntax/variable.js
@@ -6,9 +6,17 @@ console.log(username)
 
 // Try change let variable type
 
+console.log("Let Variable: ");
 let name = "Adam";
 console.log("Before change: ",name);
 name = "Bob";
 console.log("After change: ",name);
 
 
+// Try change const variable type
+console.log("Const Variable: ");
+
+const married = false;
+console.log("Before change: ",married);
+married = true; // This is error, const cant change
+console.log("After change: ",married);
