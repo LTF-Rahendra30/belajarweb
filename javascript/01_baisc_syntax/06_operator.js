07d04d3fe3703dc05f8a66df642cdcc5ae157d83
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-13 20:51:55 +0700

    try and use unary, binary, dan ternary operator.

---
+++ javascript/01_baisc_syntax/06_operator.js
@@ -0,0 +1,12 @@
+let age = 14;
+
+// Unary Operator
+typeof age;
+
+// Binary operator
+5 + 4;
+10 / 2;
+age = 30;
+
+// Ternary operator (conditional operator)
+(age < 18)? "Youre to young" : "welcome";
