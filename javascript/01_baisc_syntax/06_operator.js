748a1e4ead27a5d4b0aa24c4813208d660571e70
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-13 21:17:45 +0700

    try Logical operator , AND,OR, NOT

--- javascript/01_baisc_syntax/06_operator.js
+++ javascript/01_baisc_syntax/06_operator.js
@@ -11,7 +11,7 @@ age = 30;
 // Ternary operator (conditional operator)
 (age < 18)? "Youre to young" : "welcome";
 
-// Comparison Operator
+// Comparison Operator (==, <,>,<=,>=)
 const a = 10
 const b = 10
 const c = 20
@@ -20,3 +20,22 @@ console.log(a > c) // output: flase
 console.log(a < c) // output: ture
 
 
+// Logical Operator, 1. AND (&&) 2. OR (||), 3. NOT (!)
+
+// AND. will be fulfilled if all  conditions are true
+console.log(true && true); // true
+console.log(false && true); // false
+console.log(true && false); // false
+console.log(false && false); // false
+console.log((5 === 5) && (3 < 5)); // true
+
+// OR. will be fulfilled if one of the condistions is tue
+console.log(true || true); // true
+console.log(false || true); // true
+console.log(true || false); // true
+console.log(false || false); // false
+console.log((5 === 5) || (3 > 5)); // true
+
+// NOT
+console.log(!true); // false
+console.log(!false); // true
