3cf80ba3933a4db0fe3c3a2ae6f8db8d3c9d6ff4
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-13 21:07:59 +0700

    try comperhesion operator

--- javascript/01_baisc_syntax/06_operator.js
+++ javascript/01_baisc_syntax/06_operator.js
@@ -10,3 +10,13 @@ age = 30;
 
 // Ternary operator (conditional operator)
 (age < 18)? "Youre to young" : "welcome";
+
+// Comparison Operator
+const a = 10
+const b = 10
+const c = 20
+console.log(a == b) // output: true
+console.log(a > c) // output: flase
+console.log(a < c) // output: ture
+
+
