eee96dd1affc922143c8f1e73ad1f52426ee34f7
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-13 21:56:20 +0700

    feat: Write simple example usescase a function

--- javascript/02_functions/01_basic.js
+++ javascript/02_functions/01_basic.js
@@ -2,3 +2,14 @@ function sayHello(name) {
     result = console.log(`Hello ${name}`)
 }
 sayHello("Bob")
+
+// Example conversion Celcius to Fahrenheit
+// rules 9 / 5 * (The temperature in celcius) + 32
+
+function conversionCelciusToFahrenheit(temperature){
+    const temperatureInFahrenheit = 9 / 5 * temperature + 32
+
+    return temperatureInFahrenheit;
+}
+
+console.log(conversionCelciusToFahrenheit(90))
