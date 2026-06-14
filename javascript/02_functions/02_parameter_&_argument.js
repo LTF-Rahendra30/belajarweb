c3c93574b5bd2e94484f3c644ce1c47d70e7979c
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-14 13:31:37 +0700

    refactor: Add conditional handling in function calculate discount

--- javascript/02_functions/02_parameter_&_argument.js
+++ javascript/02_functions/02_parameter_&_argument.js
@@ -10,19 +10,25 @@ print("Hello World")
 // Example function discount price 
 
 function calculateDiscount(price,discount){
-    return price - (discount * price / 100)
+    // Conditional stetement for argument undefined
+    if (price || discount !== undefined){
+        const result = price - (discount * price / 100)
+        return result
+    }
+    return 0; // If argument is undefined
 }
 
 // Create Argument
 // Argument are data entered into function paremeters, which can be variabels or direct values
 
 // Direct
+console.log(calculateDiscount())
 console.log(calculateDiscount(1000,10)) 
 // Output: 900
 
 // Entered value by variabel
 const price = 1000; // 1000 is aargument
 const discount = 50; // 50 is argument
-console.log(calculateDiscount(price,discount)) 
+console.log(calculateDiscount(price,discount))
 // Output:
 // 500
