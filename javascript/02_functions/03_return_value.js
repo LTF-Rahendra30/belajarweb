079232900439886602ade9a822149a03d8f708a4
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-14 13:40:59 +0700

    feat: Create new file return in function

---
+++ javascript/02_functions/03_return_value.js
@@ -0,0 +1,11 @@
+
+// Simple Case
+
+function sumNumber(a,b){
+    const result = a + b;
+    return result; // This is return value that return a + b value
+}
+
+// Uses 
+console.log(sumNumber(10,90))
+// Output : 100
