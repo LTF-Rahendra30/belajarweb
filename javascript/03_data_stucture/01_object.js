acef7679a332599b4f0cf2d3e553c966e48b91f4
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-14 20:16:10 +0700

    feat: Create a simple Object

--- javascript/03_data_stucture/01_object.js
+++ javascript/03_data_stucture/01_object.js
@@ -1,3 +1,13 @@
 // Object in javascript like Dictionary in Python, Primary principle is 
 // Key and value {key : value}
 
+// Create Simple Object
+
+const user = {};
+const products = {
+    name: 'Backpack', 
+    price: 10000,
+    color : 'Black'
+
+};
+console.log(products)
