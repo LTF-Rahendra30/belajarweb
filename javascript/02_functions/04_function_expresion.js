f6a0c4b8d2b1021ef0987b4fce1c8559bea4c920
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-14 13:51:18 +0700

    fix: add missing semicolon and ensure consistent formatting in calculateTax function

--- javascript/02_functions/04_function_expresion.js
+++ javascript/02_functions/04_function_expresion.js
@@ -2,5 +2,9 @@
 
 const calculateTax = function (price,tax) {
     const result = price + (price * tax / 100);
-    return result
+    return result;
 }
+
+const calculate = calculateTax(1000,20);
+console.log(calculate)
+// output : 1200
