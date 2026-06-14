f10ed305c33841a5e19fae601f3cd2fb03331d44
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-14 20:24:38 +0700

    try accessed Object using DOt and Square Bracket

--- javascript/03_data_stucture/01_object.js
+++ javascript/03_data_stucture/01_object.js
@@ -3,11 +3,26 @@
 
 // Create Simple Object
 
-const user = {};
+const user = {
+    name: 'Bob',
+    'last name': 'Marley',
+    age: 9,
+};
+
 const products = {
     name: 'Backpack', 
     price: 10000,
-    color : 'Black'
+    color : 'Black',
 
 };
+console.log(user)
 console.log(products)
+
+// Accesed Object Properties
+
+// Using Dot
+console.log(user.name) // Output: Bob
+
+// Using square bracket
+console.log(user['age']) // Output: 9
+
