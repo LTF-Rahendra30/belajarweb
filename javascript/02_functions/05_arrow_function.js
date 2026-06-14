b34adaca91639182942d2a78e82cf2f069978616
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-14 14:09:43 +0700

    feat: Make simple exaple regular function and try it

--- javascript/02_functions/05_arrow_function.js
+++ javascript/02_functions/05_arrow_function.js
@@ -5,3 +5,5 @@ const calculateDiscountRegulerFunction = function (price,discount) {
     const result = price - (price * discount / 100)
     return result
 }
+console.log(calculateDiscountRegulerFunction(1000,50))
+// Outout: 1000 ==> 500
