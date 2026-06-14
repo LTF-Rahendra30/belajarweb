d6941eb6f5d6285739d824c6d1afb7b291aa15ea
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-14 13:18:24 +0700

    make simple example use of direct value into the function

--- javascript/02_functions/02_parameter_&_argument.js
+++ javascript/02_functions/02_parameter_&_argument.js
@@ -15,6 +15,12 @@ function calculateDiscount(price,discount){
 
 // Create Argument
 // Argument are data entered into function paremeters, which can be variabels or direct values
+
+// Direct
+console.log(calculateDiscount(1000,10)) 
+// Output: 900
+
+// Entered value by variabel
 const price = 1000; // 1000 is aargument
 const discount = 50; // 50 is argument
 console.log(calculateDiscount(price,discount)) 
