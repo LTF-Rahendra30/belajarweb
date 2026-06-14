08bca667eb0afa87434b3514e33e86d28904bc4c
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-14 14:24:51 +0700

    feat: Make simplify arrow function and try that function

--- javascript/02_functions/05_arrow_function.js
+++ javascript/02_functions/05_arrow_function.js
@@ -8,8 +8,8 @@ const calculateDiscountRegulerFunction = function (price,discount) {
 console.log(calculateDiscountRegulerFunction(1000,50))
 // Outout: 1000 ==> 500
 
-// Delclaration with Arrow Function
 
+// Delclaration with Arrow Function
 const calculateDiscountArrowFunction = (price,discount) => {
     const result = price - (price * discount / 100)
     return result
@@ -17,3 +17,26 @@ const calculateDiscountArrowFunction = (price,discount) => {
 
 console.log(calculateDiscountArrowFunction(1000,20))
 // Outout: 1000 ==> 800
+
+// REFACTOR
+
+// simplify the writing of arrow function bodies so that they dont use curly brackets at all.
+
+let calculateTax;
+
+// Arrow Function
+const calculatePriceAfterTax = (price1,tax) => {
+    const result1 = price1 + (price1 * tax / 100);
+    return result1;
+}
+
+calculateTax = calculatePriceAfterTax(1000,50);
+console.log(calculateTax);
+// Output: 1500
+
+
+// Simplify Arrow Function
+const calculatePriceAfterTaxSimplify = (price1,tax) => price1 + (price1 * tax / 100);
+calculateTax = calculatePriceAfterTaxSimplify(1000,10);
+console.log(calculateTax);
+// Output: 1100
