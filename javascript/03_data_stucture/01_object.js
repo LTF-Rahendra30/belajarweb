f0beec61a3efa23f820dba618daf9e66c0221649
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-14 20:43:12 +0700

    try change value in object and delete value in object

--- javascript/03_data_stucture/01_object.js
+++ javascript/03_data_stucture/01_object.js
@@ -34,3 +34,27 @@ console.log(name1,price )
 // Solving if key dont have value (undefined)
 const {name2, price2, soldOut = false} = products;
 console.log(soldOut); 
+
+// CHANGE VALUE IN THE OBJECT PROPERTIES
+
+const account = {
+    balance: 10000,
+    debt: 10,
+    name: "Chloe"
+};
+
+account.balance = 20000;
+console.log(account.balance)
+
+// Delete properties in the object
+
+const wallet = {
+    name: 'Bob',
+    age: 12,
+    married: false,
+};
+
+delete wallet.married;
+console.log(wallet) // Output: { name: 'Bob', age: 12 }
+delete wallet['age'] 
+console.log(wallet) // Output: { name: 'Bob'}
