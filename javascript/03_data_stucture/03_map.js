7934bbaee88678650bbe959ffb6d7e1a227778e0
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-15 11:04:27 +0700

    try create map and try Stores value in Map

--- javascript/03_data_stucture/03_map.js
+++ javascript/03_data_stucture/03_map.js
@@ -2,4 +2,25 @@
 
 // ========== CREATE MAP ===========
 
+const productMap = new Map([
+    ['shoes','Adidas'],
+    ['price',1000],
+    ['size',43]
+]);
+
+console.log(productMap)
+// Output: Map(3) { 'shoes' => 'Adidas', 'price' => 1000, 'size' => 43 }
+
+// ======== Stores Value in the Map =========
+
+// With set, first as the key, and second as the value
+
 const map = new Map();
+map.set('name','bob');
+console.log(map);
+// Output: Map(1) { 'name' => 'bob' }
+
+// With Number as the key
+map.set(1,'chloe');
+console.log(map);
+// Output: Map(2) { 'name' => 'bob', 1 => 'chloe' }
