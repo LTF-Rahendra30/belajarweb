e2bd624de4a91c57667b874228611691f3187214
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-15 23:12:09 +0700

    feat: Create new folder and new file to start learn about Control Flow

---
+++ javascript/04_control_flow/01_conditional.js
@@ -0,0 +1,13 @@
+// If Statement
+
+const gpa = 90;
+
+if (gpa >= 90){
+    console.log('Your Eligible!');
+}
+else{
+    console.log('Dont Give Up')
+}
+
+// Output: If gpa >= 90 = 'Your Eligible!'
+// And gpa <= 90 = 'Dont Give Up'
