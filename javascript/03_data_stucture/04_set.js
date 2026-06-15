df5c108617de633ccaa526dbef7c869de9c87117
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-15 11:31:29 +0700

    try stored value in set

--- javascript/03_data_stucture/04_set.js
+++ javascript/03_data_stucture/04_set.js
@@ -5,3 +5,15 @@
 const mySet = new Set([1,2,3,4]);
 console.log(mySet)
 // Output: Set(4) { 1, 2, 3, 4 }
+
+
+// ======== STORED VALUE IN SET ==========
+
+const set = new Set();
+set.add(1);
+set.add('bob');
+set.add(2);
+set.add('Andew');
+console.log(set)
+
+// Output: Set(4) { 1, 'bob', 2, 'Andew' }
