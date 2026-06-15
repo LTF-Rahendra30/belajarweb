6daa1957df27f547ee65a106a2e4f8df7bbfceb7
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-15 10:55:16 +0700

    feat: Create new file  to start learn Map Data Structure

---
+++ javascript/03_data_stucture/03_map.js
@@ -0,0 +1,5 @@
+// Map is similar to Object data structure, the main principle is (key-value) but Map can use any data type for its key, unlike Object where only String data type can be its Key.
+
+// ========== CREATE MAP ===========
+
+const map = new Map();
