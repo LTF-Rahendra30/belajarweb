03e3ce0f58d8cc05bdd914a72618c84654943af3
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-15 22:09:28 +0700

    try copy object

--- javascript/03_data_stucture/05_spread_operator.js
+++ javascript/03_data_stucture/05_spread_operator.js
@@ -6,5 +6,14 @@ const obj1 = {name: 'bob'};
 const obj2 = { lastName: 'mike', age: 13};
 const newObj = {...obj1,...obj2};
 
-console.log(newObj)
+console.log(newObj);
 // Output: { name: 'bob', lastName: 'mike', age: 13 }
+
+// spread operator combines the two objects
+
+// Copy Object
+const originalObj = {name: 'Andrew', age: 8}
+const copiedObj = {...originalObj};
+console.log(copiedObj);
+// Output: { name: 'Andrew', age: 8 }
+
