c9c7a6b0abc8fb76ee6dc5b4cd630495b5bd83b5
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-15 10:14:40 +0700

    try create Array with Constructor Object and use Array.from

--- javascript/03_data_stucture/02_array.js
+++ javascript/03_data_stucture/02_array.js
@@ -4,3 +4,21 @@
 const array = [1,2];
 console.log(typeof array); // Otput : Object
 
+// Create Array with Constructor Object
+const users = new Array();
+const numbers = new Array(5); // 5 is initial length from array
+
+console.log(users);
+console.log(numbers);
+
+// Create Array use Array.from
+
+const char = Array.from('hello');
+console.log(char);
+// Output: [ 'h', 'e', 'l', 'l', 'o' ]
+
+// This array could copy other array like this:
+const user = new Array('Andrew','Bob','Chloe');
+const customer = Array.from(user);
+console.log(customer);
+// Output: [ 'Andrew', 'Bob', 'Chloe' ]
