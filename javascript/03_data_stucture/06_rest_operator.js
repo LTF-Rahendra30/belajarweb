eabbcff4dda67f8a8e3db03a916a9f1e461644b1
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-15 22:29:36 +0700

    feat: Add function to demonstrate Array length with Rest operator

--- javascript/03_data_stucture/06_rest_operator.js
+++ javascript/03_data_stucture/06_rest_operator.js
@@ -6,3 +6,13 @@ function myFunc(...name){
 
 myFunc('Andrew','Bob','Chloe','Deny');
 // Output: Name:  [ 'Andrew', 'Bob', 'Chloe', 'Deny' ]
+
+// Array Lenght
+function users(...name2){
+    console.log(name2.length);
+    console.log('Name: ',name2);
+}
+users('Andrew','Bob','Chloe','Deny')
+// Output:
+// 4
+// Name:  [ 'Andrew', 'Bob', 'Chloe', 'Deny' ]
