0f4485bf79b2417a968723ea3a1eb90fb466af11
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-15 11:27:35 +0700

    feat: Create new file to try and learn Set data Structure

---
+++ javascript/03_data_stucture/04_set.js
@@ -0,0 +1,7 @@
+// Set dont have key and index, The data stored in the set will have unique values, meaning there will be no duplicte data
+
+// ======== CREATE SET ==========
+
+const mySet = new Set([1,2,3,4]);
+console.log(mySet)
+// Output: Set(4) { 1, 2, 3, 4 }
