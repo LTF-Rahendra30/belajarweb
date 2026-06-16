dc04c3608361ce14a64a96191ef6cc808766b9ef
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-16 16:55:21 +0700

    try Throwing Error

--- javascript/04_control_flow/03_error_handling.js
+++ javascript/04_control_flow/03_error_handling.js
@@ -1,2 +1,12 @@
 // THROWING ERROR
 
+// const error = new Error('This is Error');
+
+const price = 100;
+const paid = 80;
+if (paid < price){
+    throw new Error('not enough');
+}
+
+// CATCHING ERROR
+
