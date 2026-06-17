e5de9fea7a7523078a5690987db1483598f5990b
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-17 10:09:24 +0700

    feat: create simple variable to export and try that for export in another file

--- javascript/05_modularitation/01_import.js
+++ javascript/05_modularitation/01_import.js
@@ -1,3 +1,17 @@
+// Import Function
 import { sayHello } from "../05_modularitation/02_export.js";
 
 sayHello();
+// Output: Heloo this is Import
+
+// Import Variabels
+
+import { name,age,married } from "../05_modularitation/02_export.js";
+console.log(name);
+console.log(age);
+console.log(married);
+
+// Output:
+// Bob
+// 12
+// false
--- javascript/05_modularitation/02_export.js
+++ javascript/05_modularitation/02_export.js
@@ -1,3 +1,9 @@
 export function sayHello(){
     console.log('Heloo this is Import')
 }
+
+const name = 'Bob';
+const age = 12;
+const married = false;
+
+export {name , age, married};
