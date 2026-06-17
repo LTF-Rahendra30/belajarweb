e8fa7519a5e3085c1500311a163b404465b95d64
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-17 10:47:35 +0700

    feat: Import sayWelcome() to main.js file and try that

--- javascript/05_modularitation/main.js
+++ javascript/05_modularitation/main.js
@@ -1,1 +1,10 @@
-import { sayHello as  } from "./02_export";
+import { sayWelcome as userFunction  } from "../05_modularitation/example/02_user.js";
+
+import { sayWelcome as customerFunction } from "../05_modularitation/example/03_customer.js";
+
+
+userFunction('Bob');
+customerFunction('Chloe');
+// Output:
+// Welcome from user.js! : Bob
+// Welcome from cutomer.js! : Chloe
