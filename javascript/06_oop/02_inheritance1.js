e9f4279128b69b153288996e8bc994f62c468870
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-20 11:48:26 +0700

    feat: create perent class (smartphone)

--- javascript/06_oop/02_inheritance1.js
+++ javascript/06_oop/02_inheritance1.js
@@ -7,5 +7,15 @@ class PerentClass{
 }
 
 class ChildClass extends PerentClass{
-    
+
+}
+
+// EXAMPLE SMARTPHONE (Perent)
+
+class Smarthpone {
+    constructor(color,brand,model){
+        this.color = color;
+        this.brand = brand;
+        this.model = model;
+    }
 }
