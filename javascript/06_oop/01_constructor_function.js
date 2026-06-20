52fa17b8910947644292f8637c35d4fb9c9562c4
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-20 11:00:28 +0700

    feat: Create object and try method aplication based the objcet

--- javascript/06_oop/01_constructor_function.js
+++ javascript/06_oop/01_constructor_function.js
@@ -1,9 +1,20 @@
 function Person(name,age){
     this.name = name;
-    this.name = age;
+    this.age = age;
 
 }
 
+// The method
 Person.prototype.eat = function(){
     console.log(`${this.name} is eating`)
 }
+
+// Create Object
+const person1 = new Person('Bob',12);
+const person2 = new Person('Chloe',18);
+
+console.log(person1.name,person1.age);
+console.log(person2.name,person2.age);
+
+person1.eat();
+person2.eat();
