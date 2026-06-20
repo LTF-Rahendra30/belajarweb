2d38ed087fe4086e062c6ff9b6ab527254b5c121
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-21 00:31:33 +0700

    feat: Create Specific new Atribute in the Child Class

--- javascript/06_oop/02_inheritance2.js
+++ javascript/06_oop/02_inheritance2.js
@@ -1,10 +1,17 @@
 
-// New atribut Inheritance
-
+// Perent Class
 class Vehicle{
     constructor(model,merk,wheel){
         this.model = model;
         this.merk = merk;
         this.wheel = wheel;
+    }
+}
+
+// New atribut Inheritance with Super
+class Car extends Vehicle{
+    constructor(model,wheel,merk, numOfDorss){
+        super(merk,model,wheel); // call Constructor Perent
+        this.numOfDorss = numOfDorss; // Create Specific new Atribute in the Child Class for the car
     }
 }
