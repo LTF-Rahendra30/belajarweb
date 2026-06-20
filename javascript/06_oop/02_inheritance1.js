a228c4f246720004f1ef5ad2b7c4af6252d93520
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-20 11:53:34 +0700

    feat :  Try implementation Create object

--- javascript/06_oop/02_inheritance1.js
+++ javascript/06_oop/02_inheritance1.js
@@ -37,3 +37,13 @@ class Android extends Smarthpone{
 
     }
 }
+
+// Create Object
+const ios = new iOS('black', 'A', '12 Pro Max');
+const android = new Android('white', 'B', 'Galaxy S21');
+
+ios.charging(); // Output: Charging 12 Pro Max
+ios.airDrop(); // Output: iOS have a behavior AirDrop
+
+android.charging(); // Output: Charging Galaxy S21
+android.splitScreen(); // Output: Android have a Split Screen
