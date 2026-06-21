95c64c378810922933ef80e1766eddfa6ee0d082
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-21 22:30:42 +0700

    feat: create simple perent class and child class with overriding constructor = add new atibute at child class with super()

--- javascript/06_oop/06_polymorphism.js
+++ javascript/06_oop/06_polymorphism.js
@@ -1,3 +1,29 @@
 // ========== OVERRIDING ==========
 
 // 1. Overring Constructor
+
+// Perent Class
+class SmartPhones {
+  constructor(color, brand, model) {
+    this.color = color;
+    this.brand = brand;
+    this.model = model;
+  }
+ 
+  charging() {
+    console.log(`Charging ${this.model}`);
+  }
+}
+
+// Child Class
+class Android extends SmartPhones{
+    // overriding constructor
+    constructor(color,brand,model,device){
+        super(color,brand,model);
+        this.device = device;
+    }
+
+    splitScreen() {
+    console.log('Android have a Split Screen');
+  }
+}
