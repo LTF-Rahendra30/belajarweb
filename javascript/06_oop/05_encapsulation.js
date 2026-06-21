a2833356a6682aeb3ccb95c747b51e0883ed43d6
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-21 19:34:09 +0700

    feat: create simple class with atribute that can change

--- javascript/06_oop/05_encapsulation.js
+++ javascript/06_oop/05_encapsulation.js
@@ -1,2 +1,21 @@
 // Encapsulation is the process of wrapping and hiding data in one container (CLASS)
 
+// This Atribut class can Change in outside
+class CoffeMachine{
+    constructor(waterAmount){
+        this.waterAmount = waterAmount
+        this.temperature = 90;
+    }
+    makeCoffe(){
+        console.log(`Make a coffe at temperature ${this.temperature}`);
+    }
+}
+
+const coffe = new CoffeMachine(100);
+coffe.temperature = 20;
+
+coffe.makeCoffe();
+// Output:
+// Make a coffe at temperature 20
+// See that
+
