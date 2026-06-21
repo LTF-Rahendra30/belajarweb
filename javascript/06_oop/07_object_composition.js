f2464eae90cdf56aca794a9a6bd21e0b09524f36
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-21 23:01:38 +0700

    feat: create new file to start learn about Object Composition

---
+++ javascript/06_oop/07_object_composition.js
@@ -0,0 +1,9 @@
+// Character Game
+
+class Character{
+    constructor(name,health,posisition){
+        this.name = name;
+        this.health = health;
+        this.posisition = posisition;
+    }
+}
