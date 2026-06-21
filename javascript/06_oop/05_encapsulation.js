52f28fd841e3972bdb7e3f2e67fa8817d2addcdf
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-21 19:42:32 +0700

    feat: create new class with Get and Set method, and try change some atribute in the class.

--- javascript/06_oop/05_encapsulation.js
+++ javascript/06_oop/05_encapsulation.js
@@ -19,3 +19,30 @@ coffe.makeCoffe();
 // Make a coffe at temperature 20
 // See that
 
+console.log("-".repeat(40));
+// Get and Set method
+
+class CoffeMachine2{
+    constructor(waterAmount){
+        this.waterAmount  = waterAmount;
+        this._temperature = 90;
+    }
+
+    // Set method
+    set temperature(temperature){
+        console.log("you're not all allowed to change the temperature");
+    }
+    // Get method
+    get temperature(){
+        return this._temperature;
+    }
+}
+
+const coffe2 = new CoffeMachine2(10);
+console.log("Berofe change: ",coffe2.temperature);
+coffe2.temperature = 1000;
+console.log("Adfter change: ",coffe2.temperature)
+// Output:
+// Berofe change:  90
+// you're not all allowed to change the temperature
+// Adfter change:  90
