8ec4e04c8419b64e3ec0b72c8c52d47b8b6f2fe9
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-21 23:05:04 +0700

    feat: Create perent class included child class and some method

--- javascript/06_oop/07_object_composition.js
+++ javascript/06_oop/07_object_composition.js
@@ -6,4 +6,28 @@ class Character{
         this.health = health;
         this.posisition = posisition;
     }
+
+    canMove(){
+        console.log(`${this.name} moves to ${this.posisition}`);
+    }
+}
+
+// ============= CHILD CLASS ===========
+ 
+class Monster extends Character {
+  canAttack() {
+    console.log(`${this.name} attacks with a weapon!`);
+  }
+}
+ 
+class Guardian extends Character {
+  canDefend() {
+    console.log(`${this.name} defends with a shield!`);
+  }
+}
+ 
+class Wizard extends Character {
+  canCastSpell() {
+    console.log(`${this.name} casts a magic spell!`);
+  }
 }
