64436ac935299e32d36bd30d197235b66191dc98
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-21 17:56:59 +0700

    feat: Create second child class

--- javascript/06_oop/04_inheritance3.js
+++ javascript/06_oop/04_inheritance3.js
@@ -24,3 +24,18 @@ iOS.prototype.airDrop = function() {
   console.log('iOS have a behavior AirDrop');
 };
 
+
+// Create Child Class
+
+function Android(color,brand,model){
+    SmartPhones(this,color,brand,model);
+}
+
+Android.prototype = Object.create(SmartPhones.prototype);
+Android.prototype.constructor = Android;
+
+// Method
+Android.prototype.splitScreen = function(){
+    console.log('Android have a Split Screen');
+}
+
