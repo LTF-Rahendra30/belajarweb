e4d3c865113c538913f468dbcadb74da90867788
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-22 09:42:50 +0700

    feat: Create simple pure function

---
+++ javascript/07_functional_programming/02_pure_function.js
@@ -0,0 +1,20 @@
+// Pure function always produces the same output every time it is called using the same arguments and the results are predictable
+
+// Pure function has no side effect
+
+// Example Pure Function
+
+function addWith(value,addingValue){
+    return value + addingValue;
+}
+
+const result1 = addWith(0,1);
+console.log(`result1 is ${result1}`); // Output: result1 is 1
+
+const result2 = addWith(result1, 1);
+console.log(`result2 is ${result2}`); // Output: result2 is 2
+
+const result3 = addWith(result2, 1);
+console.log(`result3 is ${result3}`); // Output: result3 is 3
+
+console.log(result1, result2, result3); // Output: 1, 2, 3
