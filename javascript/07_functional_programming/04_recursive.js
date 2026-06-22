1d2199ca3d68aff4b9052186ddd830c1bd2f8e44
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-22 13:58:24 +0700

    feat: Create function generate array

--- javascript/07_functional_programming/04_recursive.js
+++ javascript/07_functional_programming/04_recursive.js
@@ -1,1 +1,12 @@
 // a function calls itself
+
+// Example
+function generateArray(n){
+    const result = [];
+    for (let counter =0; counter <=n; counter+=1){
+        result.push(counter);
+    }
+    return result;
+}
+
+console.log(generateArray(5)); // Output: [0, 1, 2, 3, 4, 5]
