6979ab265849a9a7f9397e7cbcb48d28869ce80e
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-22 22:45:19 +0700

    feat: create a function with recursive and try that

--- javascript/07_functional_programming/04_recursive.js
+++ javascript/07_functional_programming/04_recursive.js
@@ -10,3 +10,14 @@ function generateArray(n){
 }
 
 console.log(generateArray(5)); // Output: [0, 1, 2, 3, 4, 5]
+
+// But in Functional Programming, we cant change the data, so we cant change the value of the counter variabel, to solved this problem, do that:
+
+function generateArray2(n){
+    if(n<0){
+        return [];
+    }
+    return [...generateArray2(n-1),n]
+}
+
+console.log(generateArray(5)); // Output: [0, 1, 2, 3, 4, 5]
