38dcea66fbe1d2a1449d7fd585663452e2d35e73
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-22 10:00:56 +0700

    feat: Create simple example function

--- javascript/07_functional_programming/03_high_order_fucntion.js
+++ javascript/07_functional_programming/03_high_order_fucntion.js
@@ -1,1 +1,22 @@
 // HOF is a function that accepts another function as an argument and/or returns another function.
+
+// Example
+
+function apply(opertion,...agrs){
+    return opertion(...agrs)
+}
+
+function sum(a,b,c){
+    return a+b+c;
+}
+
+function discount(disc,value){
+    return value - ((disc / 100)  * value);
+
+}
+
+const productPrice = apply(sum,100,100,200);
+const withDiscount = apply(discount,25,productPrice);
+
+console.log('Product price:', productPrice); // Output: Product price: 400
+console.log('With discount 25%:', withDiscount); // Output: With discount 25%: 300
