23be6fb31e89a1aedb7d60b30dd6af6d1c19a11a
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-22 23:10:45 +0700

    feat: implement pure function for email registration using spread operator

--- javascript/07_functional_programming/05_immunntability.js
+++ javascript/07_functional_programming/05_immunntability.js
@@ -13,3 +13,25 @@ const personWithEmail = registerEmail(person, 'bob@gmail.com');
 
 console.log(person); //{ name: 'bob', username: 'Boby', email: 'bob@gmail.com' }
 console.log(personWithEmail); //{ name: 'bob', username: 'Boby', email: 'bob@gmail.com' }
+
+// ====== THE SOLUTION ABOUT THIS PROBLEM TO BE MAKE THE FUNCTION TO  PURE WITH SPREAD OPERATOR ========
+
+function registerEmail2(person,email){
+    // Use spread operator for duplicate person value
+    return{...person,email};
+}
+
+const person2 = {
+    name: 'Chloe-un',
+    username: 'chloe',
+};
+
+const personWithEmail2 = registerEmail2(person2,'chloe@mail.com')
+
+
+console.log(person2);
+// { name: 'Chloe-un', username: 'chloe' }
+
+
+console.log(personWithEmail2);
+// { name: 'Chloe-un', username: 'chloe', email: 'chloe@mail.com' }
