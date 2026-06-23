5e3aa283994f57ac27502e20ad1fd68207b2503e
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-23 17:22:45 +0700

    feat: Add synchronous and asynchronous data download examples with console outputs

--- javascript/08_asynchronous/01_basic.js
+++ javascript/08_asynchronous/01_basic.js
@@ -1,3 +1,5 @@
+
+// Sync
 function ambilDataSync() {
   console.log("1. Start download data (Sync)...");
   // Simulasi proses macet / memblokir waktu selama 3 detik
@@ -6,7 +8,24 @@ function ambilDataSync() {
   console.log("2. Done, Successed Download! (Sync)");
 }
 
+console.log("=== PROSES SYNC ===");
 ambilDataSync();
 // Output:
 // 1. Start download data (Sync)...
 // 2. Done, Successed Download! (Sync), printed after 2 second
+
+
+// Async
+
+function downloadSimulation(){
+    return new Promise(resolve => setTimeout(() => resolve("2. Done, Successed Download! (Async)"),3000));
+}
+
+async function addDataAsync() {
+    console.log("1.Start Download (Async)");
+    const result = await downloadSimulation();
+    console.log(result);
+    
+}
+console.log("=== PROSES ASYNC ===");
+addDataAsync();
