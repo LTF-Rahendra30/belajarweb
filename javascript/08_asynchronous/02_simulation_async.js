d299c41a5a0e21ef5edd514f6ec88ad48b52b079
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-23 18:22:20 +0700

    feat: Update coffee simulation to include completion messages

--- javascript/08_asynchronous/02_simulation_async.js
+++ javascript/08_asynchronous/02_simulation_async.js
@@ -2,8 +2,15 @@
 
 const estimationTime = 3000; // 3 Second
 
-setTimeout(() =>{
-    console.log('Hello World');
-},estimationTime);
+// setTimeout(() =>{
+//     console.log('Hello World');
+// },estimationTime);
 
 // Hello world will printed on terminal after 3 second 
+
+
+// Import
+import { makeCoffe } from "./mod/01_modul.js";
+
+console.log("I ordered coffe at the cafe")
+makeCoffe();
