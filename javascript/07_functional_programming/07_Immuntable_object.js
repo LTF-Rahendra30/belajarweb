8547b594038c33579286b31aa63785d73356cc2c
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-23 12:58:44 +0700

    feat: create deep freeze funtion and simple explanation about that

--- javascript/07_functional_programming/07_Immuntable_object.js
+++ javascript/07_functional_programming/07_Immuntable_object.js
@@ -18,3 +18,19 @@ user.email = 'Bob32@gmail.com';
 console.log(user);
 // { name: 'Bob', email: 'Bob12@gmail.com' }
 // This is Immuntable
+
+// But, Object freeze must freezed first section form the object, If the object has other properties, that properties can change.
+
+// To make Object to be pure IMMUNTABLE Oject, we will do freezed every properties of the object with RECURSION
+
+
+function deepFreeze(object){
+    Object.keys(object).forEach((name) => {
+        const prop = object[name];
+        if (typeof prop === 'object' && prop !== null){
+            deepFreeze(prop);
+        }
+    });
+    return Object.freeze(object);
+
+}
