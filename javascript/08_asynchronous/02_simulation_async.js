83499e54f3d2b9fcbf354328b679c05fd604e634
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-23 17:57:35 +0700

    feat: Simulation Async manual with setTimeout()

--- javascript/08_asynchronous/02_simulation_async.js
+++ javascript/08_asynchronous/02_simulation_async.js
@@ -1,2 +1,9 @@
 // Simulation with setTimeout()
 
+const estimationTime = 3000; // 3 Second
+
+setTimeout(() =>{
+    console.log('Hello World');
+},estimationTime);
+
+// Hello world will printed on terminal after 3 second 
