30492f8f500589d95b41ce24ec0210946cb80818
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-23 12:30:06 +0700

    feat: Add filtering example for students based on scholarship eligibility

--- javascript/07_functional_programming/06_immuntability_array_method.js
+++ javascript/07_functional_programming/06_immuntability_array_method.js
@@ -13,3 +13,28 @@ console.log(newArray); // [ 'Andrew New', 'Bob New', 'Chloe New' ]
 const turthyArray = [1, '', 'Halo', 0, null, 'Harry', 14].filter((item) => Boolean(item));
 
 console.log(turthyArray); //[ 1, 'Halo', 'Harry', 14 ]
+
+// Filtering array from object 
+const students = [
+    {
+        name: 'Bob',
+        score:60,
+    },
+    {
+        name: 'Chloe',
+        score: 88,
+    },
+    {
+        name: 'Ron',
+        score: 90,
+    },
+    {
+        name: 'Bethy',
+        score: 75,
+    }
+];
+
+const eligibleForScholarhip = students.filter((student) => student.score > 85);
+console.log(eligibleForScholarhip);
+// Output:
+// [ { name: 'Chloe', score: 88 }, { name: 'Ron', score: 90 } ]
