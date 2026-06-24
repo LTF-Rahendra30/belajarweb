4fd72472c44eaa558dc34133e45d3a2be16700c2
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-24 22:24:15 +0700

    feat: Create function exceutor and export that function. Also return value of promise object

---
+++ javascript/08_asynchronous/mod/utils.mjs
@@ -0,0 +1,21 @@
+function promiseExecutor(resolve,reject){
+    setTimeout(()=> {
+        console.log("Do something before before Promise has done");
+
+        // Result of Async Procces
+        const number = Math.random();
+
+        // value fulfilled
+        if (number>0.5){
+            resolve("You did it")
+        }
+
+        else{
+            reject("Sorry, something went wrong")
+        }
+    },2000);
+}
+
+export function doSomething(){
+    return new Promise(promiseExecutor);
+}
