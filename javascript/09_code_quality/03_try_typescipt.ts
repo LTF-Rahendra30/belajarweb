be12007adaf711c691df4ff7d15f6e83bc54d535
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-25 16:21:46 +0700

    feat: Try make simple function

--- javascript/09_code_quality/03_try_typescipt.ts
+++ javascript/09_code_quality/03_try_typescipt.ts
@@ -4,6 +4,20 @@ function greet(name: string){
     console.log(`Hello ${name}. My name is ${myName}`);
 }
 
+const age: number = 12;
+
 greet("JavaScript")
 // Output:
 // Hello JavaScript. My name is TypeScipt
+
+
+function adding(numA: number, numB: number): number {
+    return numA + numB;
+}
+
+const result = adding(10,10);
+console.log(result) // 20
+
+// Try add string and number
+const result2 = adding('10',10);
+console.log(result2) // 1010
