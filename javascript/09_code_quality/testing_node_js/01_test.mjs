2696f15d1565c176c2df725f5dd7e7e39492160d
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-25 17:14:23 +0700

    feat:  Try testing string as a parameter the function

--- javascript/09_code_quality/testing_node_js/01_test.mjs
+++ javascript/09_code_quality/testing_node_js/01_test.mjs
@@ -13,4 +13,47 @@ test("should add correctly", ()=>{
     assert.equal(actualValue,expectedvalue)
 });
 
-// Ouput: ✔ should add correctly (0.8373ms)
+// Ouput: 
+/* ✔ should add correctly (0.8373ms)
+
+ℹ tests 1
+ℹ suites 0
+ℹ pass 1
+ℹ fail 0
+ℹ cancelled 0
+ℹ skipped 0
+ℹ todo 0
+ℹ duration_ms 9.0444 */
+
+// =========== Try String as a Parameters function ==========
+
+test("Should throw an error if string passed on numA parameter", () => {
+  const potentialErrorToBeThrew = () => {
+    // Arrange
+    const operandA = '5';
+    const operandB = 4;
+
+    // Action
+    add(operandA, operandB);
+  };
+
+  // Assert
+  assert.throws(potentialErrorToBeThrew, Error);
+});
+
+// Output:
+/* 
+✖ failing tests:
+
+test at 01_test.mjs:30:1
+✖ Should throw an error if string passed on numA parameter
+
+{
+    generatedMessage: false,
+    code: 'ERR_ASSERTION',
+    actual: undefined,
+    expected: [Function],
+    operator: 'throws',
+    diff: 'simple'
+  }
+*/
