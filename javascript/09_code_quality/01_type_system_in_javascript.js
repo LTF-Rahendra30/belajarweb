8287a3e916febd2537366421ed9c8ec5f0e0dad2
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-25 11:24:47 +0700

    feat: Create simple example data type declaration

--- javascript/09_code_quality/01_type_system_in_javascript.js
+++ javascript/09_code_quality/01_type_system_in_javascript.js
@@ -1,1 +1,21 @@
 // JavaScript is a dynamically typed language that determines data types at runtime.
+
+// Example
+
+let myNum = 0;
+myNum = 1;
+
+console.log(myNum); // 1
+
+myNum = true;
+console.log(myNum) // true
+
+// The Problem
+
+function add(numA,numB){
+    return numA + numB;
+}
+
+console.log(add(1, 1)); // 2
+console.log(add(3, 2)); // 5
+console.log(add('5', 4)); // ??? ==> 54
