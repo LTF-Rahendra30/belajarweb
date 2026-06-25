3198a265406e25c97c1e8bbc657f325fead14dc7
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-25 21:39:11 +0700

    feat: Add initial testing setup for calculator functionality

---
+++ javascript/09_code_quality/testing_node_js/04_testing_bun.mjs
@@ -0,0 +1,23 @@
+
+// import {it,describe,expect} from "bun:test";
+
+// function add(numA, numB) {
+//   return numA + numB;
+// }
+ 
+// describe('Calculator', () => {
+//   it('should add correctly', () => {
+//     // Arrange
+//     const operandA = 1;
+//     const operandB = 1;
+ 
+//     // Action
+//     const actualValue = add(operandA, operandB);
+ 
+//     // Assert
+//     const expectedValue = 2;
+//     expect(actualValue).toBe(expectedValue);
+//   });
+// });
+
+console.log(5 === '5');
