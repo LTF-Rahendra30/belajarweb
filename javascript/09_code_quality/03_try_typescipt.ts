beea5a7941a865120a39a359a1971970e25b1274
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-25 16:10:36 +0700

    feat: Add new file TypeScript to try that

---
+++ javascript/09_code_quality/03_try_typescipt.ts
@@ -0,0 +1,9 @@
+const myName: string = "TypeScipt";
+
+function greet(name: string){
+    console.log(`Hello ${name}. My name is ${myName}`);
+}
+
+greet("JavaScript")
+// Output:
+// Hello JavaScript. My name is TypeScipt
